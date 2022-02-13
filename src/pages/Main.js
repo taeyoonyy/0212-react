f7a496045460043b720e61f3969553793f5e4bd4
taeyoonyy <tyyu@lsis.com>
2022-02-14 00:25:07 +0900

    add form, useRef in main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import classes from './Main.module.css'
+import React from "react";
+import classes from "./Main.module.css";
+import { useRef } from "react";
 
-const main = () => {
+const Main = () => {
+  const nameInputRef = useRef();
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+    console.log("CLICK", nameInputRef.current.value);
+  };
+
   return (
     <React.Fragment>
-      <div className={classes.header}> MAIN PAGE</div>
-      <form className={classes.form}>
-        <label>NAME</label>
-        <input id="name" type="text"></input>
-        <button>Login</button>
-      </form>
+      <div className={classes.main}>
+        <div className={classes.header}> MAIN PAGE</div>
+        <form className={classes.form} onSubmit={loginHandler}>
+          <label>NAME</label>
+          <input id="name" type="text" ref={nameInputRef}></input>
+          <button>Login</button>
+        </form>
+      </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default main;
+export default Main;
