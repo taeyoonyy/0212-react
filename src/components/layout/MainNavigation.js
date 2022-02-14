db191a009cd98243f2f76878e362ec0488e7dbd4
taeyoonyy <tyyu@lsis.com>
2022-02-14 22:01:13 +0900

    add main navigation

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react/cjs/react.production.min";
+import classes from "./MainNavigation.module.css";
+
+const MainNavigation = () => {
+  return (
+    <Fragment>
+      <div className={classes.navi}>
+        <p>Hello</p>
+      </div>
+    </Fragment>
+  );
+};
+
+export default MainNavigation;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,10 @@
 import { Fragment } from "react"
+import MainNavigation from "../components/layout/MainNavigation";
 
 const Main = () => {
   return (
     <Fragment>
+      <MainNavigation />
       <h1>MAIN PAGE</h1>
     </Fragment>
   )
