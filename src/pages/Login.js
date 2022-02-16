2c4035e7e8f58ce7685fc20ca540f35ffbf7fff9
taeyoonyy <tyyu@lsis.com>
2022-02-17 00:46:21 +0900

    use error modal in login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import classes from "./Login.module.css";
 import { useRef } from "react";
 import { useHistory } from "react-router-dom";
-
 import UserContext from "../store/user-context";
+import ErrorModal from "../components/ui/ErrorModal"
 
 const Login = () => {
   const nameInputRef = useRef();
   const history = useHistory();
-
   const userCtx = useContext(UserContext);
+  const [error, setError] = useState();
 
   const loginHandler = (event) => {
     event.preventDefault();
@@ -17,11 +17,21 @@ const Login = () => {
     if (!!nameInputRef.current.value) {
       userCtx.setUser(nameInputRef.current.value)
       history.push('/main')
+    } else {
+      setError({
+        title: "Error",
+        message: "Please enter the Name"
+      });
     }
   };
 
+  const errorHandler = () => {
+    setError(null);
+  }
+
   return (
     <React.Fragment>
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
       <div className={classes.login}>
         <div className={classes.header}> LOGIN PAGE</div>
         <form className={classes.form} onSubmit={loginHandler}>
