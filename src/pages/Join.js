026a594ef5b564a3cd9c8ddb1a5b0da539f3e685
taeyoonyy <tyyu@lsis.com>
2022-02-19 01:57:58 +0900

    add error modal in join page

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -3,15 +3,21 @@ import { useHistory } from "react-router-dom";
 import { useState } from "react";
 import classes from './Join.module.css'
 import Button from '../components/ui/Button'
+import ErrorModal from "../components/ui/ErrorModal";
 
 const Join = () => {
   const history = useHistory();
   const [email, setEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState(null);
+  const [error, setError] = useState(false);
 
   const joinHandler = (event) => {
     event.preventDefault();
-    history.push("/login");
+    if (emailIsValid) {
+      history.push("/login");
+    } else {
+      setError(true);
+    }
   };
 
   const changeEmailHandler = (event) => {
@@ -22,9 +28,14 @@ const Join = () => {
   const validateEmailHandler = (event) => {
     setEmailIsValid(event.target.value.includes("@"));
   };
+  
+  const errorModalHandler = () => {
+    setError(false);
+  }
 
   return (
     <Fragment>
+      {error && <ErrorModal title={"ERROR"} message={"Please check input data"} onConfirm={errorModalHandler}></ErrorModal>}
       <h1>JOIN PAGE</h1>
       <form onSubmit={joinHandler}>
         <div className={`${classes.control} ${
